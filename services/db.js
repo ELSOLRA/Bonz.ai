4488904d61d75235c16ee98cb750f3a472e27e15
ELSOLRA <bardakasmiljo@gmail.com>
2024-09-16 13:21:29 +0200

    package.json with required node modules, rum.json for load to data base, and services/db.js for initializing data base

---
+++ services/db.js
@@ -0,0 +1,10 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient } = require("@aws-sdk/lib-dynamodb");
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION,
+});
+
+const db = DynamoDBDocumentClient.from(client);
+
+module.exports = db;
