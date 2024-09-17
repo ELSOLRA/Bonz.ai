845b620b8fcf1214d17bd5b7107c0b444ac180e2
MapYh <145336421+MapYh@users.noreply.github.com>
2024-09-18 00:39:30 +0200

    Added starting code for a way to cancel orders.

--- functions/cancelOrder/cancelOrder.js
+++ functions/cancelOrder/cancelOrder.js
@@ -1,23 +1,33 @@
-
 const db = require("../../services/db.js");
-
-
+const { DeleteCommand } = require("@aws-sdk/lib-dynamodb");
 
 exports.cancelOrder = async (event) => {
-const {bookingNumber} = event.body
+  const orderTable = process.env.ORDER_TABLE;
+  const { id } = JSON.parse(event.body);
 
-
-  const params = {
-     TableName: "order",
-    Key: {
-        bookingnumber: `${bookingNumber}`,
-    }
+  const deleteParams = {
+    id: `${id}`,
   };
 
   try {
-    
-   
-  } catch (err) {
-    
+    let result = await db.send(
+      new DeleteCommand({
+        TableName: orderTable,
+        Item: deleteParams,
+      }),
+    );
+    if (result) {
+      return apiResponse(200, { message: "Booking canceled successfully." });
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Booking not canceled." }),
+      };
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
   }
 };
